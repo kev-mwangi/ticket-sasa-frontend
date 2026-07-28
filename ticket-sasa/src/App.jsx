deaa64bd9a979c93233444ddbd6800f05ab11c3e
Kelvin <kevinmwas007@gmail.com>
2026-07-29 01:16:26 +0300

    feat:composed the app.jsx

--- ticket-sasa/src/App.jsx
+++ ticket-sasa/src/App.jsx
@@ -1,3 +1,62 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import EventDetail from "./pages/EventDetail.jsx";
+import EventForm from "./pages/EventForm.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import MyTickets from "./pages/MyTickets.jsx";
+import Signup from "./pages/Signup.jsx";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route
+          path="/my-tickets"
+          element={
+            <ProtectedRoute>
+              <MyTickets />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute role="organizer">
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/new"
+          element={
+            <ProtectedRoute role="organizer">
+              <EventForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/edit/:id"
+          element={
+            <ProtectedRoute role="organizer">
+              <EventForm />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
 
 
 
